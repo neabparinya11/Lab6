7a87be70b96642e941ad1ffc9242b657be13c2ea
lab314-46@labeng.local <lab314-46@labeng.local>
2020-12-25 16:25:42 +0700

    complete konngou

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,29 @@
-//Dialog of the first example is given below.
+#include <iostream>
+using namespace std;
+int main() {
+	string x,z,a,b;
+	int y;
+cout <<"Fahsai: Sawadee ka...Can you tell me your name?"<<endl;
+cout<<"?????: ";
+getline(cin,x);
+cout<<"Fahsai: Wow!!! "<<x<<" is a really cool name."<<endl;
+cout<<"Fahsai: I think you are an Engineering student. What is your student ID?"<<endl;
+cout<<x<<": ";
+cin>>y;
+cin.ignore();
+int year=(y/10000000)-12;
+cout<<"Fahsai: I think you may be GEAR "<<year<<". I have a free movie ticket for you."<<endl;
+cout<<"Fahsai: Let's go to the cinema together!!!"<<endl;
+cout<<"Fahsai: What movie do you want to watch?"<<endl;
+cout<<x<<": ";
+getline(cin,z);
+cout<<"Fahsai: So....which day are you free to go with me?"<<endl;
+cout<<x<<": ";
+getline(cin,a);
+cout<<"Fahsai: "<<a<<"....that is OK!!! I'm looking forward to watching "<<z<<" with you."<<endl;
+cout<<x<<": ";
+getline(cin,b);
+cout<<"Fahsai: 555+ see you "<<a<<". Bye Bye \\(^ ^)/"<<endl;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+    return 0;
+}
